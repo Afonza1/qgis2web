f56a98ad2f8fecc7ae3761f680228cfeec794a82
Afonza1 <AFONZA2@mail.ru>
2026-05-31 22:34:28 +0900

    initial_commit

--- billdinghousebuilding_house_4_style.js
+++ billdinghousebuilding_house_4_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_highway_3 = function(feature, resolution){
+var style_billdinghousebuilding_house_4 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -21,7 +21,7 @@ var style_highway_3 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(141,90,153,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,158,23,1.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
